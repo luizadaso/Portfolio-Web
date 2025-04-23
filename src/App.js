5ffa9a91589e9c79c153c201a24a9d3f6f978ae5
Ana Luiza <analuiza.daso@gmail.com>
2025-04-23 17:58:33 -0300

    Testando outra forma de importação do css para resolver o problema de o CSS não constar no deploy.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'src/App.css';
+import style from './App.css'; // Importação do estilo usando a sintaxe desejada
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import SobreMim from './pages/SobreMim';
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <div>
-        <Navbar className="barraSuperior" bg="dark" data-bs-theme="dark">
+        <Navbar className={style.barraSuperior} bg="dark" data-bs-theme="dark">
           <Container className="justify-content-center">
             <Nav className="mx-auto">
               <Nav.Link as={Link} to="/">Página inicial</Nav.Link>
@@ -39,44 +39,44 @@ function App() {
 function SocialIcons() {
   return (
     <div className="d-flex flex-column flex-md-row justify-content-center align-items-center">
-      <div className="iconesRedes">
-        <a href="https://www.linkedin.com/in/luizadaso/" className="no-underline" target="_blank" rel="noopener noreferrer">
+      <div className={style.iconesRedes}>
+        <a href="https://www.linkedin.com/in/luizadaso/" className={style.noUnderline} target="_blank" rel="noopener noreferrer">
           <img
             src={linkedin}
             alt="linkedin"
-            className="img-fluid linkedin-icon ms-1"
+            className={`img-fluid ${style.linkedinIcon} ms-1`}
           />
-          <span className="titulos-icones ms-2 d-inline d-md-none">Meu perfil no Linkedin</span>
+          <span className={`titulos-icones ms-2 d-inline d-md-none`}>Meu perfil no Linkedin</span>
         </a>
       </div>
-      <div className="iconesRedes">
-        <a href="mailto:analuiza.daso@gmail.com" className="no-underline">
+      <div className={style.iconesRedes}>
+        <a href="mailto:analuiza.daso@gmail.com" className={style.noUnderline}>
           <img
             src={mail}
             alt="e-mail"
-            className="img-fluid email-icon ms-1"
+            className={`img-fluid ${style.emailIcon} ms-1`}
           />
-          <span className="titulos-icones ms-2 d-inline d-md-none">analuiza.daso@gmail.com</span>
+          <span className={`titulos-icones ms-2 d-inline d-md-none`}>analuiza.daso@gmail.com</span>
         </a>
       </div>
-      <div className="iconesRedes">
-        <a href="https://api.whatsapp.com/send?phone=5522999955542&text=Ola,+Encontrei+o+seu+portfolio.+Podemos+conversar?" className="no-underline" target="_blank" rel="noopener noreferrer">
+      <div className={style.iconesRedes}>
+        <a href="https://api.whatsapp.com/send?phone=5522999955542&text=Ola,+Encontrei+o+seu+portfolio.+Podemos+conversar?" className={style.noUnderline} target="_blank" rel="noopener noreferrer">
           <img
             src={telefone}
             alt="telefone"
-            className="img-fluid telefone-icon ms-1"
+            className={`img-fluid ${style.telefoneIcon} ms-1`}
           />
-          <span className="titulos-icones ms-2 d-inline d-md-none">(22) 99995-5542</span>
+          <span className={`titulos-icones ms-2 d-inline d-md-none`}>(22) 99995-5542</span>
         </a>
       </div>
-      <div className="iconesRedes">
-        <a href="https://github.com/luizadaso" className="no-underline" target="_blank" rel="noopener noreferrer">
+      <div className={style.iconesRedes}>
+        <a href="https://github.com/luizadaso" className={style.noUnderline} target="_blank" rel="noopener noreferrer">
           <img
             src={github}
             alt="github"
-            className="img-fluid github-icon ms-1"
+            className={`img-fluid ${style.githubIcon} ms-1`}
           />
-          <span className="titulos-icones ms-2 d-inline d-md-none">Repositório no Github</span>
+          <span className={`titulos-icones ms-2 d-inline d-md-none`}>Repositório no Github</span>
         </a>
       </div>
     </div>
@@ -92,7 +92,7 @@ function ConditionalFooter() {
   }
 
   return (
-    <footer className="rodape bg-dark text-white text-center py-3">
+    <footer className={`rodape bg-dark text-white text-center py-3`}>
       <Container>
         <span>Desenvolvido por @luizadaso</span>
       </Container>
