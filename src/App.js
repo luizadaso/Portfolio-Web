899172a663ed93e6c87d6079a1472e38c678b716
Ana Luiza <analuiza.daso@gmail.com>
2025-05-06 18:16:53 -0300

    Adicionar rodapé no body de todas as páginas.

--- src/App.js
+++ src/App.js
@@ -30,6 +30,11 @@ function App() {
           <Route path="/projetos" element={<Projetos />} />
         </Routes>
         <SocialIcons />
+        <Container className="text-center py-3">
+          <span style={{ color: 'gray' }}>
+            Desenvolvido por @luizadaso <br/> Todos os direitos reservados &copy; {new Date().getFullYear()}
+          </span>
+        </Container>
         <ConditionalFooter />
       </div>
     </Router>
