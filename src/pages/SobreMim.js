f7b67d4c6113e639cd4d0c5adf2adab6c13905cb
Ana Luiza <analuiza.daso@gmail.com>
2025-05-06 17:34:47 -0300

    Revisão do texto sobre mim.

--- src/pages/SobreMim.js
+++ src/pages/SobreMim.js
@@ -10,42 +10,43 @@ function SobreMim() {
         </Card.Header>
           <Card.Body>
             <Card.Text>
-            Desde a infância, sempre tive uma paixão por tecnologia, especialmente pelos jogos. Entretanto, as limitações
-            financeiras da minha família significavam que, enquanto meus amigos já desfrutavam de um PlayStation 1, eu me
-            contentava com um "polystation", que me proporcionava muitas horas de diversão. Havia um considerável atraso
-            na adoção de novas tecnologias em minha casa.Quando via profissionais da área de tecnologia na televisão, 
-            parecia um sonho distante. Sempre ouvi que era necessário ser extremamente inteligente para atuar nesse 
-            campo, o que me levou a acreditar que apenas os superdotados poderiam trabalhar em tecnologia. Dessa forma,
+            Desde a infância, sempre tive uma paixão por tecnologia, especialmente pelos jogos. Porém, as limitações
+            financeiras da minha família significavam que, enquanto meus amigos já possuíam um PlayStation 1, eu me
+            tinha um PolyStation, que me divertia muito. Havia um considerável atraso
+            na adoção de novas tecnologias em minha casa. Quando via profissionais da área de tecnologia na televisão, 
+            parecia algo impossível de se alcançar. Sempre ouvi que era necessário ser extremamente inteligente para atuar nesse 
+            campo, o que me levou a acreditar que apenas os superdotados poderiam trabalhar com programação. Dessa forma,
             nunca considerei essa possibilidade para mim.  <br /><br /> 
-            O tempo passou e a pergunta que sempre ecoou na minha mente – "O que eu quero ser quando crescer?" – 
+            O tempo passou e a pergunta que sempre ecoou na minha mente: "O que eu quero ser quando crescer?" 
             continuava sem resposta. Eu deixava a vida me levar, aceitando as oportunidades que surgiam, mas sem um 
-            objetivo profissional claro. Acabei me acomodando em um cargo confortável como suporte técnico de TI, mesmo 
-            sem cursos ou experiência na área. Na minha cidade, havia escassez de profissionais qualificados, e eu, mesmo 
-            sem formação, consegui uma das vagas. Esse foi o meu primeiro contato profissional com TI, onde atendi grandes
+            objetivo profissional claro. Acabei conquistando e me acomodando em um cargo confortável como técnica de suporte em TI, 
+            mesmo sem cursos ou experiência na área. Na minha cidade, havia escassez de profissionais qualificados, e eu, mesmo 
+            sem formação, consegui uma dessas vagas. Esse foi o meu primeiro contato profissional com TI, onde atendi grandes
             clientes. O trabalho era presencial, mas o atendimento ao cliente era remoto. Aprendi a resolver problemas 
             simples e complexos, sempre utilizando a abordagem de tentativa e erro, buscando informações e testando 
-            soluções. Eu gostava do que fazia. A rotina era confortável: trabalhava seis horas por dia em uma escala 6x1,
+            soluções. Eu gostava do que fazia. A rotina era tranquila: trabalhava 6h por dia em uma escala 6x1,
             rodeada por uma equipe unida e divertida.
-            <br /><br /> No entanto, a falta de perspectivas profissional me incomodava. Percebi que o suporte técnico tinha um limite
-            muito restrito para crescimento, algo que, na minha visão, era insatisfatório. Sempre sonhei em grande, mas o
+            <br /><br /> No entanto, a falta de perspectivas profissional me incomodava. Percebi que o suporte técnico tinha um 
+            limite muito restrito para crescimento, algo que, na minha visão, era insatisfatório. Sempre sonhei grande, mas o 
             vazio da indefinição profissional me atormentava. <br /><br />
             Em dezembro de 2023, candidatei-me a uma vaga na área em que já atuava como suporte técnico, buscando uma 
             oportunidade home office. Após várias candidaturas e reformulações do meu perfil no LinkedIn, fui convidada 
             para uma entrevista. Nela, foi sugerido que eu iniciasse uma graduação para ser efetivada em um nível pleno; 
-            caso contrário, seria efetivada como nível júnior. Aceitei o desafio e, no início de 2024, comecei a graduação
-            em Ciência da Computação.  <br /><br /> Esse novo desafio se tornou um divisor de águas em minha vida. Despertei para o 
-            desenvolvimento de software e percebi que, sim, eu poderia entrar nessa área. A partir desse momento, 
-            busquei cursos e me aprofundei em um mundo novo, onde posso criar sistemas e softwares que alcançam 
-            proporções inimagináveis.
+            caso contrário, seria efetivada como nível júnior. Aceitei o desafio e, no início de 2024, iniciei a graduação de Bacharel
+            em Ciência da Computação.  <br /><br /> Esse novo desafio se tornou um divisor de águas em minha vida. Despertei um 
+            enorme interesse em desenvolvimento de software e percebi que, sim, EU Posso/Quero/Vou entrar nessa área. A partir desse momento, 
+            busquei cursos e estou cada dia mais me aprofundando em um mundo novo, onde posso criar sistemas e softwares que 
+            alcançam proporções inimagináveis. O mundo da programação! <br /><br />
             Após sete anos de experiência no suporte técnico de TI, finalmente encontrei um objetivo profissional com o
             qual me conecto e que sinto que vale a pena todo o esforço e comprometimento. Vejo que posso ir longe e 
-            alcançar grandes conquistas.
+            alcançar grandes objetivos.
             <br /><br /> Atualmente, estou em busca da minha transição para a área de desenvolvimento de software. Estar imersa neste
             novo universo de infinitas possibilidades gera uma sensação de grandeza e motivação que nunca experimentei 
-            antes. É incrível ter várias ideias e concretizá-las. Continuo criando projetos pessoais e enfrentando
+            antes. É incrível ter várias ideias e poder concretizá-las. Continuo criando projetos pessoais e enfrentando
             desafios. A longa curva de aprendizado é maravilhosa, pois percebo que nunca haverá um ponto final. 
-            <br /> Esta área é para a vida toda, sempre haverá algo novo e incrível para aprender. <br /><br />
-            Ana Luiza Oliveira
+            <br /> Programação é para a vida toda, sempre haverá algo novo e incrível para aprender. <br /><br />
+            Todos os mistérios são ciências que ainda não foram descobertas. <br /><br />
+            Ana Luiza Da S. Oliveira
             </Card.Text>
           </Card.Body>
       </Card>
